c37e004d2d9784938fb991e5f4719640e4809adb
morantg <morantgeorges02@gmail.com>
2018-12-03 22:05:09 +0100

    adding a feature to allow the user to log in if a password is lost

--- register.php
+++ register.php
@@ -50,7 +50,7 @@ if(!empty($_POST)){
 
 	$user_id = $pdo->lastInsertId();
 
-	mail($_POST['email'], "confirmation compte","afin de valider votre compte merci de cliquer sur ce lien\n\nhttp://localhost/Comptes/confirm.php?id=$user_id&token=$token");
+	mail($_POST['email'], "confirmation compte","afin de valider votre compte merci de cliquer sur ce lien\n\nhttp://localhost/p5_blog/Comptes/confirm.php?id=$user_id&token=$token");
 	$_SESSION['flash']['success'] = 'email envoyé';
 	header('Location: login.php');
 	exit();
